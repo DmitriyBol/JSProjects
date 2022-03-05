8797fba053b0dc71fc0f8dc406b2aeb08b59db6c
Dmitriy B <patrik.pjemslav@gmail.com>
2022-03-05 16:37:40 +0500

    m added func 05032022

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {ChangeEvent, SyntheticEvent, useState} from 'react';
+import './style/main.scss'
 
 function App() {
+    const [isSelected, setSelected] = useState('Главная');
+
+    const setSelectedMenu = (e: any): void => {
+        setSelected(e.target.innerHTML)
+    }
+
     return (
-        <div>
-            <header>
-                Навигация
+        <div className='main-container'>
+            <header className='header-main'>
+                <nav>
+                    <ul className='nav-list'>
+                        <li className='nav-list_item'>
+                            <a
+                            style={isSelected === 'Главная' ? {backgroundColor: '#6acbff'} : {}}
+                            href="#"
+                            onClick={setSelectedMenu}>Главная
+                            </a>
+                        </li>
+                        <li className='nav-list_item'>
+                            <a
+                                style={isSelected === 'Магазин' ? {backgroundColor: '#6acbff'} : {}}
+                                href="#"
+                                onClick={setSelectedMenu}
+                            >Магазин</a>
+                        </li>
+                        <li className='nav-list_item'>
+                            <a
+                                style={isSelected === 'Контакты' ? {backgroundColor: '#6acbff'} : {}}
+                                href="#"
+                                onClick={setSelectedMenu}
+                            >Контакты
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
-            <section className='left-cont'> Основное поле с товарами и картинками </section>
-            <section className='right-cont'> Меню, фильтры </section>
+            <section className='left-cont'> Основное поле с товарами и картинками</section>
+            <section className='right-cont'> Меню, фильтры</section>
             <footer>
                 дополнительная инфа
             </footer>
